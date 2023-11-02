ffb62622c804827c35ef5d6c9b71e30f7dca54cb
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-11-02 21:05:47 +0530

    updates

--- blog/src/Components/Home/Home.js
+++ blog/src/Components/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import toast from "react-hot-toast";
 import api from "../../ApiConfig";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
   const [allBlogs, setAllBlogs] = useState([]);
@@ -50,81 +51,99 @@ const Home = () => {
   }, [title, categoryValue]);
 
   return (
-    <div id="home-screen">
-      <div id="home-header">
-        <div id="home-header-left">
-          <div id="search">
-            <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-            <input
-              type="text"
-              placeholder="Search blogs..."
-              onChange={handleSearchValue}
-              value={title}
+    <>
+      <div id="home-screen">
+        <div id="home-header">
+          <div id="home-header-left">
+            <div id="search">
+              <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+              <input
+                type="text"
+                placeholder="Search blogs..."
+                onChange={handleSearchValue}
+                value={title}
+              />
+            </div>
+          </div>
+          <div id="home-header-right">
+            <div id="blogs-category">
+              <p>Category :</p>
+              <select
+                name="filter"
+                onChange={handleCategoryValue}
+                value={categoryValue}
+              >
+                <option>All</option>
+                {filterBlogs?.length &&
+                  filterBlogs?.map((blog) => (
+                    <>
+                      <option key={blog._id}>{blog?.category}</option>
+                    </>
+                  ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div id="home-body">
+          <div id="all-blogs">
+            {allBlogs?.length ? (
+              allBlogs?.map((blog) => (
+                <div
+                  id="single-blog"
+                  key={blog._id}
+                  onClick={() => navigateTo(`/single-blog/${blog._id}`)}
+                >
+                  <div id="main-img">
+                    <div id="blog-img">
+                      <img src={blog.image1} alt="blog" />
+                    </div>
+                  </div>
+                  <div id="blog-details">
+                    <div id="blog-title">
+                      <h4>{blog.title}</h4>
+                    </div>
+                    <div id="blog-desc">
+                      <p>
+                        {blog.description1.slice(0, 200)}... <b>Read More</b>
+                      </p>
+                    </div>
+                    <div id="blog-category">
+                      <button>{blog.category}</button>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div id="no-blogs">
+                <div id="no-blogs-msg">
+                  <h1>NO BLOGS FOUND!</h1>
+                </div>
+              </div>
+            )}
+          </div>
+          {/* <FontAwesomeIcon icon="fa-solid fa-circle-chevron-left" /> */}
+        </div>
+      </div>
+
+      <div id="arrows-screen">
+        <div id="arrows">
+          <div id="left-arrow">
+            {" "}
+            <FontAwesomeIcon
+              icon="fa-solid fa-chevron-left"
+              style={{ fontSize: "40px" }}
+            />
+          </div>
+          <div id="right-arrow">
+            {" "}
+            <FontAwesomeIcon
+              icon="fa-solid fa-chevron-right"
+              style={{ fontSize: "40px" }}
             />
           </div>
         </div>
-        <div id="home-header-right">
-          <div id="blogs-category">
-            <p>Category :</p>
-            <select
-              name="filter"
-              onChange={handleCategoryValue}
-              value={categoryValue}
-            >
-              <option>All</option>
-              {filterBlogs?.length &&
-                filterBlogs?.map((blog) => (
-                  <>
-                    <option key={blog._id}>{blog?.category}</option>
-                  </>
-                ))}
-              {/* <option>All</option>
-              <option>Food</option>
-              <option>Technology</option>
-              <option>Automobiles</option> */}
-            </select>
-          </div>
-        </div>
       </div>
-      <div id="home-body">
-        <div id="all-blogs">
-          {allBlogs?.length ? (
-            allBlogs?.map((blog) => (
-              <div
-                id="single-blog"
-                key={blog._id}
-                onClick={() => navigateTo(`/single-blog/${blog._id}`)}
-              >
-                <div id="main-img">
-                  <div id="blog-img">
-                    <img src={blog.image1} alt="blog" />
-                  </div>
-                </div>
-                <div id="blog-details">
-                  <div id="blog-title">
-                    <h4>{blog.title}</h4>
-                  </div>
-                  <div id="blog-desc">
-                    <p>
-                      {blog.description1.slice(0, 200)}... <b>Read More</b>
-                    </p>
-                  </div>
-                  <div id="blog-category">
-                    <button>{blog.category}</button>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div id="no-blogs">
-              <div id="no-blogs-msg">
-                <h1>NO BLOGS FOUND!</h1>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
